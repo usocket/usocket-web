e0a62ccd10899b192d8eacadedca48904e987387
Chun Tian (binghe) <binghe.lisp@gmail.com>
2018-08-30 12:10:12 +0200

    [release] usocket-0.7.1

--- releases/release.sh
+++ releases/release.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -x
 
-rm -rf usocket-$1/.git
-tar cvzf usocket-$1.tar.gz usocket-$1
+# tarball is from GitHub releases
+
 rm usocket-latest.tar.gz
 ln -s usocket-$1.tar.gz usocket-latest.tar.gz
 gpg -b -a usocket-$1.tar.gz
